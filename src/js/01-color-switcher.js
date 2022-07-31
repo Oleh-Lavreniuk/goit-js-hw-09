4bbb724c5ed43b7a062d74901b88dad1e989baa2
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-07-31 18:12:51 +0300

    add 1-st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+startBtn.addEventListener('click', onStartChangeColor);
+stoptBtn.addEventListener('click', onStopChangeColor);
+
+let intervalId = null;
+let isActive = false;
+
+function onStartChangeColor() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    console.log('Color is change', intervalId);
+  }, 1000);
+}
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+  console.log('Stop change color', intervalId);
+  isActive = false;
+}
