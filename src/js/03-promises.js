11c5815643b10497b3abb39e3db927f8d6f3d340
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-08-05 00:59:51 +0300

    add base to task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delayInp: document.querySelector('[name="delay"]'),
+  stepInp: document.querySelector('[name="step"]'),
+  amountInp: document.querySelector('[name="amount"]'),
+  submitBtn: document.querySelector('button'),
+};
+
+// console.log('refs', refs.delay);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +17,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
