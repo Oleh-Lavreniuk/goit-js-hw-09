de012cf2a4b78acafffe966910ede28da50e8021
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-08-07 00:42:16 +0300

    add task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,45 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  delayInp: document.querySelector('[name="delay"]'),
-  stepInp: document.querySelector('[name="step"]'),
-  amountInp: document.querySelector('[name="amount"]'),
-  submitBtn: document.querySelector('button'),
-};
-
-// console.log('refs', refs.delay);
+const btnRef = document.querySelector('[type="submit"]');
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', handleSubmitForm);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+function handleSubmitForm(ev) {
+  ev.preventDefault();
+  btnRef.disabled = true;
+
+  const step = +ev.currentTarget.elements.step.value;
+  const amount = +ev.currentTarget.elements.amount.value;
+  let delay = +ev.currentTarget.elements.delay.value;
+  // console.log(ev.currentTarget.elements);
+
+  setTimeout(() => {
+    btnRef.disabled = false;
+  }, amount * step + delay);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise  ${position} in ${delay}ms`);
+      });
+
+    delay += step;
+  }
+}
